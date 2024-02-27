943a34eb53c1f6ec4a6672aacde48221309c9e23
djhi12 <ido20002@byui.edu>
2024-02-27 14:17:53 +0800

    updated

--- concepts/arrayInt/Program.cs
+++ concepts/arrayInt/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Simple for loop to print numbers from 1 to 5
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
